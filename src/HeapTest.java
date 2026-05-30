07eb6931554311e3d9b3a8fb1f8b49290e412e2d
leoraggy <leoragual35@gmail.com>
2026-05-30 11:06:51 -0700

    Added test for isEmpty method

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -29,12 +29,26 @@ public class HeapTest {
 
 
     @Test
-    public void testSize(){
+    public void testIsEmptyWithElements(){
         Heap minHeap = new Heap();
 
-        int expected = 0;
-        int actual = minHeap.size();
+        minHeap.add(5);
+
+        boolean expected = false;
+        boolean actual = minHeap.isEmpty();
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testIsEmptyNoElements(){
+        Heap minHeap = new Heap();
+
+        boolean expected = true;
+        boolean actual = minHeap.isEmpty();
+
+        assertEquals(expected, actual);
+    }
+
+    
 }
